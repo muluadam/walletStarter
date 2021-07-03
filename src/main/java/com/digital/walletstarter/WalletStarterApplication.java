8f5df456c8ff5ac7a78c30db829fd0fe78652f22
muluadam <muluadamt@gmail.com>
2021-07-03 18:53:26 -0500

    fc

---
+++ src/main/java/com/digital/walletstarter/WalletStarterApplication.java
@@ -0,0 +1,13 @@
+package com.digital.walletstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digital/walletstarter/WalletStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.walletstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

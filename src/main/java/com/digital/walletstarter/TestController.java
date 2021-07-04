ce5837d70d6b4fc378ed9bd7b2d02d3b4fa7b012
muluadam <muluadamt@gmail.com>
2021-07-03 19:41:22 -0500

    welcome

---
+++ src/main/java/com/digital/walletstarter/TestController.java
@@ -0,0 +1,35 @@
+package com.digital.walletstarter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test2")
+    public String testTwo(){
+        return "Test2 m to from github ";
+    }
+    @GetMapping("/hello")
+    public String testTwos(){
+        return "Hello from Github";
+    }
+    @GetMapping("/hello2")
+    public String testTwos2(){
+        return "Hello method two is changed sdf ";
+    }
+
+    @GetMapping("/hello3")
+    public String test3(){
+        return "Hello  added ";
+    }
+    @GetMapping("/welcom")
+    public String welcom(){
+        return "Hello  welcom to DW ";
+    }
+    @GetMapping("/")
+    public String test4(){
+        return "Hello! Done  ";
+    }
+
+
+}
